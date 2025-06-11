dd22d64f623b42e3d0914db45c70dc4076a19e95
Juho Siitonen <juho_tuomas@msn.com>
2025-06-12 01:03:20 +0300

    new component to handle all the algorithm function calls

---
+++ dungeon-generator/src/components/algorithms/generateDungeon.ts
@@ -0,0 +1,20 @@
+import type { DungeonMapMatrix, RoomSpecifics } from "../types";
+import { createCorridorsFromMST} from "./aStar";
+import { createMapAndRooms } from "./createMapAndRooms";
+import { delaunayTriangulation } from "./delaunayTriangulation";
+import { getMSTLines, primsAlgorithm } from "./minimumSpanningTree";
+import type { Point, Triangle } from "./types";
+
+// Tämä funktio kutsuu muita algoritmeja luodakseen luolaston, jossa on huoneita ja käytäviä.
+// corridorType ei ole vielä käytössä, mutta ajattelin vielä tehdä jonkin muun kuin A* algoritmin käytäville
+export const generateDungeon = (
+  numberOfRooms: number, seed: string, _corridorType: 'astar', allowDiagonalCorridors: boolean, manualRooms?: RoomSpecifics[]
+): { roomSpecifics: RoomSpecifics[]; map: DungeonMapMatrix; triangulation: Triangle[]; mstEdges: Array<{start: Point, end: Point}>} => {
+  const { roomSpecifics, map } = manualRooms ? createMapAndRooms(manualRooms.length, seed, manualRooms) : createMapAndRooms(numberOfRooms, seed);
+  const triangles = delaunayTriangulation(roomSpecifics)
+  const mst = primsAlgorithm(triangles)
+  const mstEdges: Array<{start: Point, end: Point}> = getMSTLines(mst)
+  const mapWithRoomsAndCorridors = createCorridorsFromMST(map, mstEdges, allowDiagonalCorridors)
+  
+  return { roomSpecifics, map: mapWithRoomsAndCorridors, triangulation: triangles, mstEdges };
+}
